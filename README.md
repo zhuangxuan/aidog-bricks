cdb2651352f97c3cf9d375e882baa6d59b68188c
Andy-庄绪安 <790280811@qq.com>
2024-01-31 10:57:00 +0800

    dd

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 * 单元测试
 * 提交发布前验证
 * travis 实现自动发布
+
+
+
+
+https://docs.npmjs.com/cli/v10/using-npm/scripts
+
+内置  "prepublishOnly": "npm run lint && npm run test&& npm run build"
+在运行npm publish之前运行 prepublishOnly
--- initGit.sh
+++ initGit.sh
@@ -2,7 +2,11 @@
 
 echo "git init"
 git init ;
+echo "git add .;"
 git add .;
+echo "git commit -m "first commit";"
 git commit -m "first commit";
+echo "git remote add origin git@gitee.com:tmzzxa/lego-bricks.git;"
 git remote add origin git@gitee.com:tmzzxa/lego-bricks.git;
-git push -u origin "master"
+echo "git push --set-upstream origin main"
+git push --set-upstream origin main
